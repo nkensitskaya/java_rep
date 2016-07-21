00ec42482e1c83792a56444efd44acbfdb30411a
nkensitskaya <irpin83@yandex.ru>
2016-07-21 23:36:01 +0300

    Home work 2
    1. Point class added
    2. Distance function added
    3. Distance method added

--- sandbox/src/main/java/test/sandbox/pack/MyProgram.java
+++ sandbox/src/main/java/test/sandbox/pack/MyProgram.java
@@ -4,6 +4,16 @@ public class MyProgram {
 
 	public static void main(String[] args){
 		System.out.println("Hello, world!");
+
+		Point A = new Point(0,0);
+		Point B = new Point(3,3);
+		System.out.println("Distance between A and B = " + distance(A,B));
+		System.out.println("Distance between A and B = " + A.distance(B));
+
 	}
 
+
+	public static double distance(Point p1,Point p2){
+		return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
+	}
 }
---
+++ sandbox/src/main/java/test/sandbox/pack/Point.java
@@ -0,0 +1,18 @@
+package test.sandbox.pack;
+
+
+public class Point {
+
+    public double x;
+    public double y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point p){
+        return Math.sqrt((p.x-this.x)*(p.x-this.x)+(p.y-this.y)*(p.y-this.y));
+    }
+
+}
