64340c5dac62d56abd4013dabd15ca0e61ba87b6
nkensitskaya <irpin83@yandex.ru>
2016-07-27 21:29:11 +0300

    AddContact methods were moved to TestBase class

--- addressbook/src/test/java/test/addressbook/pack/TestBase.java
+++ addressbook/src/test/java/test/addressbook/pack/TestBase.java
@@ -71,4 +71,44 @@ public class TestBase {
     public void tearDown() {
         wd.quit();
     }
+
+    protected void returnToContactList() {
+        wd.findElement(By.linkText("home page")).click();
+    }
+
+    protected void submitNewContact() {
+        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    }
+
+    protected void fillContactForm(ContactData contactData) {
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
+        wd.findElement(By.name("middlename")).click();
+        wd.findElement(By.name("middlename")).clear();
+        wd.findElement(By.name("middlename")).sendKeys(contactData.getMiddleName());
+        wd.findElement(By.name("lastname")).click();
+        wd.findElement(By.name("lastname")).clear();
+        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
+        wd.findElement(By.name("nickname")).click();
+        wd.findElement(By.name("nickname")).clear();
+        wd.findElement(By.name("nickname")).sendKeys(contactData.getNickname());
+        wd.findElement(By.name("title")).click();
+        wd.findElement(By.name("title")).clear();
+        wd.findElement(By.name("title")).sendKeys(contactData.getTitle());
+        wd.findElement(By.name("address")).click();
+        wd.findElement(By.name("address")).clear();
+        wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
+        wd.findElement(By.name("home")).click();
+        wd.findElement(By.name("home")).clear();
+        wd.findElement(By.name("home")).sendKeys(contactData.getPhoneHome());
+        wd.findElement(By.name("email")).click();
+        wd.findElement(By.name("email")).clear();
+        wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+    }
+
+    protected void initNewContactCreation() {
+
+        wd.findElement(By.linkText("add new")).click();
+    }
 }
